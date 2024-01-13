f7155dc614e4a7e21934b5d736fd91815a8c0c28
celeranis <contact@celeranis.dev>
2024-01-13 14:29:01 -0600

    Worlds
    * Generator for Simualted Universe/Worlds

---
+++ src/util/Table.ts
@@ -0,0 +1,95 @@
+export function whitespace(string: string, targetWhitespace: number = 13) {
+	return string + ' '.repeat(targetWhitespace - string.length)
+}
+
+export function multilineFormat(cell: string | number) {
+	if (typeof cell == 'string' && cell.includes('\n'))
+		return '\n' + cell
+	else
+		return typeof cell == 'string' && cell.startsWith('!') ? cell : ' ' + cell
+}
+
+export class Table {
+	data: (string | number)[][] = []
+	
+	constructor(public customClass: string, public header?: string[]) {
+		
+	}
+	
+	addRow<T extends (string | number)[]>(...args: T) {
+		const row = [...args]
+		this.data.push(row)
+		return row
+	}
+	
+	addRows(...rows: (string | number)[][]) {
+		this.data.push(...rows)
+	}
+	
+	wikitable(inlineRows = true) {
+		const output = [`{| class="${this.customClass}"`]
+		
+		if (this.header) {
+			if (inlineRows) {
+				output.push(`! ${this.header.join(' !! ')}`)
+			} else {
+				output.push(...this.header.map(header => `! ${header}`))
+			}
+		}
+		
+		for (const row of this.data) {
+			output.push('|-')
+			if (inlineRows && !row.find(cell => typeof cell == 'string' && cell.includes('\n'))) {
+				output.push(`| ${row.join(' || ')}`)
+			} else {
+				output.push(...row.map(cell => `${cell.toString().startsWith('!') ? '' : '|'}${multilineFormat(cell)}`))
+			}
+		}
+		
+		output.push('|}')
+		
+		return output.join('\n')
+	}
+	
+	get row_count() {
+		return this.data.length + (this.header ? 1 : 0)
+	}
+
+	get col_count() {
+		return Math.max(...this.data.map(row => row.length))
+	}
+	
+	wrapper() {
+		const output = [
+			`{{Table Wrapper`,
+			`|class        = ${this.customClass}`,
+			`|size         = ${this.row_count};${this.col_count}`
+		]
+		let currentRow = 1
+		if (this.header) {
+			output.push(...this.header.map((cell, i) => `|${whitespace(`header-${currentRow}-${i + 1}`)}= ${cell}`))
+			currentRow++
+		}
+		
+		for (const row of this.data) {
+			output.push(...row.map((cell, i) => `|${whitespace(`cell-${currentRow}-${i + 1}`)}=${multilineFormat(cell)}`))
+			currentRow++
+		}
+		
+		output.push('}}')
+		
+		return output.join('\n')
+	}
+	
+	html() {
+		const output = ['<table class="article table">']
+		
+		if (this.header) {
+			output.push(
+				'\t<thead>',
+				...this.header.map(cell => `\t\t<th>${cell}</th>`),
+				'\t</thead>'
+			)
+		}
+	}
+}
