e06d663fa00015c99acfd261efb7defb00c13ebd
celeranis <contact@celeranis.dev>
2024-01-16 16:21:39 -0600

    Allow referencing a local copy of the game files in config

--- src/files/GameFile.ts
+++ src/files/GameFile.ts
@@ -1,3 +1,4 @@
+import { readFile } from 'fs/promises';
 import config from '../../config.json' with { "type": "json" };
 import { VERSION_COMMITS } from '../Shared.js';
 
@@ -8,6 +9,10 @@ export class HttpError extends Error {
 }
 
 export async function getFile<T extends object>(path: string, version: string = config.target_version): Promise<T> {
+	if (config.local_roots[version]) {
+		return JSON.parse((await readFile(`${config.local_roots[version]}/${path}`)).toString())
+	}
+	
 	const data = await fetch(`${config.repo_root}${VERSION_COMMITS[version] || version}/${path}`)
 		.catch(err => {
 			console.error(`Error while fetching ${path}`)
